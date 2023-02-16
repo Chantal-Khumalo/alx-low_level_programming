84590c9bdc393cdb5345aea09e98c184547648e5
Chantal-Khumalo <chantalkhumalo957@gmail.com>
2023-02-16 15:29:17 -0800

    Size is not grandeur, and territory does not make a notion

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - A program that prints size of various types
+ * Return 0
+ */
+
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("Size of a char: %zu byte(s)\n", (unsigned long)
+sizeof(a));
+printf("Size of a int: %zu byte(s)\n", (unsigned long)
+sizeof(b));
+printf("Sizee of a long int: %zu byte(s)\n", (unsigned long)
+sizeof(c));
+printf("Size of a long long int: %zu byte(s)\n", (unsigned long)
+sizeof(d));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)
+sizeof(e));
+return (0);
+}
